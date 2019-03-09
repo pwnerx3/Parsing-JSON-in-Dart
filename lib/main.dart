218e1cac0dbadc79e5a898ed6a33141bb623dc21
pwnerx3 <francisco.casiano2@upr.edu>
2019-03-09 11:18:23 -0400

    App now waits for dndguide to be loaded, and then shows its value instead of originally having to hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+//------------------------------------------------------------------------ Fetches the JSON file database and initializes the dndguide variable
   Future<String> _loadAsset() async {
     return await rootBundle.loadString('assets/data/HDARG.json');
   }
@@ -34,28 +34,44 @@ class _MyHomePageState extends State<MyHomePage> {
     dndguide = jsonResponse;
   }
 
+//------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-    loadjson();
     var scrollcontroller;
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height - 24,
-              margin: EdgeInsets.only(top: 24.0),
-              width: MediaQuery.of(context).size.width * .90,
-              child: SingleChildScrollView(
-                physics: BouncingScrollPhysics(),
-                controller: scrollcontroller,
-                scrollDirection: Axis.vertical,
-                child: Text(dndguide.toString()),
+    return Container(
+//------------------------------------------------------------------------ Waits fot the asynchronous, Future, functions to provide either completion or failure calls
+      child: FutureBuilder(
+        future: loadjson(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              backgroundColor: Colors.black,
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height - 24,
+                      margin: EdgeInsets.only(top: 24.0),
+                      width: MediaQuery.of(context).size.width * .90,
+                      child: SingleChildScrollView(
+                        physics: BouncingScrollPhysics(),
+                        controller: scrollcontroller,
+                        scrollDirection: Axis.vertical,
+                        child: Text(dndguide.toString(),
+                        style: TextStyle(color: Colors.white),),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
+            );
+          } else if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
