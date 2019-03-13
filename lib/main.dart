6bdfed2cfbb8e40eacf4c531bb8be385ed18974e
pwnerx3 <francisco.casiano2@upr.edu>
2019-03-13 16:06:15 -0400

    IT WOOORRKKSSSS but only when I tested the names of the races... Not with the descriptions... Besides that I changed the pubspec.yaml in order for the JSON to be found and also reformatted the JSON for better legibility

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,21 @@ import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+//------------------------------------------------------------------------ All arrays & variables that will be used
 
-var dndguide;
+  List races;
+  List ractraits;
+  List rtdesc;
+  List traits;
+  List tdesc;
+  String test;
+//------------------------------------------------------------------------ Fetches JSON and deals with anything related to it
+
+
+//------------------------------------------------------------------------
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -22,57 +34,44 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-//------------------------------------------------------------------------ Fetches the JSON file database and initializes the dndguide variable
-  Future<String> _loadAsset() async {
-    return await rootBundle.loadString('assets/data/HDARG.json');
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+
+    Future<String> _loadjson() async {
+    var response = await rootBundle.loadString('assets/data/HDARG.json');
+
+
+    setState(() {
+     var extracteddata = json.decode(response);
+     races = extracteddata['races'];
+    });
   }
 
-  Future loadjson() async {
-    String jsonString = await _loadAsset();
-    final jsonResponse = json.decode(jsonString);
-    dndguide = jsonResponse;
+  @override
+  void initState() {
+    _loadjson();
   }
 
 //------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-    var scrollcontroller;
-    return Container(
-//------------------------------------------------------------------------ Waits fot the asynchronous, Future, functions to provide either completion or failure calls
-      child: FutureBuilder(
-        future: loadjson(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Scaffold(
-              backgroundColor: Colors.black,
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      height: MediaQuery.of(context).size.height - 24,
-                      margin: EdgeInsets.only(top: 24.0),
-                      width: MediaQuery.of(context).size.width * .90,
-                      child: SingleChildScrollView(
-                        physics: BouncingScrollPhysics(),
-                        controller: scrollcontroller,
-                        scrollDirection: Axis.vertical,
-                        child: Text(dndguide.toString(),
-                        style: TextStyle(color: Colors.white),),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Parsed races'),
+      ),
+      body: ListView.builder(
+        itemCount: races == null ? 0 : races.length,
+        itemBuilder: (BuildContext context, i){
+          return Container(
+             child:Row( 
+               children:[
+                 Text(races[i]['name']),
+                 ],
+             )
+          );
         },
       ),
     );
-  }
+          
+}
 }
