85b864f707c0ba13adfe926b55571357b4da545c
pwnerx3 <francisco.casiano2@upr.edu>
2019-03-14 12:00:58 -0400

    It now works. Added a bonus description key to the json and from now on will use this json format

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'dart:async' show Future;
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
 
 void main() {
   runApp(MyApp());
+  debugPaintSizeEnabled = true;
 }
 //------------------------------------------------------------------------ All arrays & variables that will be used
 
-  List races;
-  List ractraits;
-  List rtdesc;
-  List traits;
-  List tdesc;
-  String test;
+List races;
+List ractraits;
+List rtdesc;
+List traits;
+List tdesc;
+String test;
 //------------------------------------------------------------------------ Fetches JSON and deals with anything related to it
-
 
 //------------------------------------------------------------------------
 
@@ -35,43 +36,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-
-    Future<String> _loadjson() async {
+  Future<String> _loadjson() async {
     var response = await rootBundle.loadString('assets/data/HDARG.json');
 
-
     setState(() {
-     var extracteddata = json.decode(response);
-     races = extracteddata['races'];
+      var extracteddata = json.decode(response);
+      races = extracteddata['races'];
     });
   }
 
   @override
   void initState() {
+    super.initState();
     _loadjson();
   }
 
 //------------------------------------------------------------------------
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Parsed races'),
       ),
-      body: ListView.builder(
-        itemCount: races == null ? 0 : races.length,
-        itemBuilder: (BuildContext context, i){
-          return Container(
-             child:Row( 
-               children:[
-                 Text(races[i]['name']),
-                 ],
-             )
-          );
-        },
+      body: Center(
+        child: ListView.builder(
+          itemCount: races == null ? 0 : races.length,
+          itemBuilder: (BuildContext context, i) {
+            return ListTile(
+              title: Text(races[i]['name']),
+              subtitle: Text(races[i]['bonusdescription']),
+            );
+          },
+        ),
       ),
     );
-          
-}
+  }
 }
